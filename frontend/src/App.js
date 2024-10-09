2a849dbc04ad2d0560ffa17dfbc802dcded02108
tmotswenyane023b <tmotswenyane023b@student.wethinkcode.co.za>
2024-10-09 14:29:27 +0200

    designing the navigation_bar(WORKING ON FRONTEND)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <Greet/>
-      <Welcome/>
+      {/* <Welcome/> */}
     </div>
   );
 }
--- frontend/src/components/Greet.js
+++ frontend/src/components/Greet.js
@@ -1,9 +1,28 @@
-import React from 'react'
-const Greet = () => <h1>Hello Thabang</h1>
+import React from 'react';
+import './myComponents.css'; // Make sure to import your CSS file
+import { FaHome, FaSearch, FaPlusSquare, FaHeart, FaUser } from 'react-icons/fa';
 
-// function Greet()
-// {
-//     return <h1>GREETINGS THABANG</h1>
-// }
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <img 
+          // src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" 
+          alt="Logo" 
+        />
+      </div>
+      <div className="navbar__search">
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="navbar__menu">
+        <a href="#"><FaHome /></a>
+        <a href="#"><FaSearch /></a>
+        <a href="#"><FaPlusSquare /></a>
+        <a href="#"><FaHeart /></a>
+        <a href="#"><FaUser /></a>
+      </div>
+    </nav>
+  );
+}
 
-export default Greet;
+export default NavBar;
